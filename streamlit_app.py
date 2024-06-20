cc3f2ab6feec0f21258ad046c789d2995b4688c8
plataformasft <90005765+plataformasft@users.noreply.github.com>
2024-06-19 20:14:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import zipfile
 
 # Page title
 st.set_page_config(page_title='ML Modelador', page_icon='🤖')
-st.title('🤖 ML Model Building')
+st.title('🤖 ML Modelador')
 
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
